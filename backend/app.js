b6d17f2da5839f661bd6ba37e1505a7644086f12
Mir <mir.m.ali01@gmail.com>
2023-04-11 09:51:12 -0400

    setup mongoose/mongo connection

--- backend/app.js
+++ backend/app.js
@@ -14,20 +14,20 @@ const Exercise = require("./models/exercise");
 const Workout = require("./models/workout");
 
 // middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(methodOverride("_method"));
-// app.use((req, res, next) => {
-//   console.log(req.originalUrl);
-//   next();
-// });
-// mongoose.set("strictQuery", true);
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// mongoose.connection.once("open", () => {
-//   console.log("Connected to MongoDB");
-// });
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.set("App.js", "src/App.js");
 
